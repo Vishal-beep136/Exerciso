f9020019069942c6374dc6071e279cb1eb84a1bb
Vishal Beep <alaphtech@gmail.com>
2022-06-12 16:18:48 +0530

    initial commit

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+const SearchExercises = () => {
+  return (
+    <Stack alignItems="center" mt="37px"
+      justifyContent="center" p='20px'>
+
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        mb="50px" textAlign="center">
+        Awsome Exercises <br /> You Should Know
+      </Typography>
+
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: {
+              fontWeight: '700',
+              border: 'none',
+              borderRadius: '4px'
+            },
+            width: { lg: '700px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px'
+          }}
+          height="76px" value=""
+          onChange={(e) => { }}
+          placeholder="Search Exercises"
+          type="text" />
+
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0'
+          }}
+          onClick={() => { }}>Search</Button>
+
+      </Box>
+
+    </Stack>
+  )
+}
+
+export default SearchExercises
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  )
+}
+
+export default Home
