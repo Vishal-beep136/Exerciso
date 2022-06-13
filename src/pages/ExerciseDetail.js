474bdf96eeec7feed5107e3ec06a97a95ad0571f
Vishal Beep <alaphtech@gmail.com>
2022-06-13 11:28:43 +0530

    added exercise videos from yt

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, youtubeOptions, fetchData } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilerExercises from '../components/SimilerExercises';
@@ -9,6 +9,7 @@ import SimilerExercises from '../components/SimilerExercises';
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const { id } = useParams();
+  const [exerciseVideos, setExerciseVideos] = useState([])
 
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -18,7 +19,9 @@ const ExerciseDetail = () => {
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       setExerciseDetail(exerciseDetailData);
 
-
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
+        youtubeOptions);
+      setExerciseVideos(exerciseVideosData.contents);
     }
     fetchExercisesData()
   }, [id]);
@@ -26,7 +29,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       <SimilerExercises />
     </Box>
   )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
     }
 };
 
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    }
+};
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = response.json();
