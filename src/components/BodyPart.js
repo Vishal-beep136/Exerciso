d5d20ce5dd0c442effa81f005c11846334de4fda
Vishal <82146140+Vishal-beep136@users.noreply.github.com>
2022-06-17 09:43:35 +0530

    Update BodyPart.js

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,24 +3,6 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png';
 
-// const BodyPart = (item, bodyPart, setBodyPart) => {
-//     return (
-//         <Stack
-//             type='button'
-//             alignItems='center'
-//             justifyContent='center'
-//             className='bodyPart-card'
-//             sx={
-//                 bodyPart === item ? {
-//                     borderTop: "4px solid #ff2625",
-//                     backgroundColor: "#fff"
-//                 } : ''
-//             }
-//         >
-//             <img src={Icon} alt="dumbbel" style={{ width: "40px", height: "40px" }} />
-//         </Stack >
-//     )
-// }
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => (
     <Stack
